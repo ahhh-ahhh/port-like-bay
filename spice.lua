ff00d4f4fee94e06b60da183dfe9f39365b72c59
AHHH <64360573+ahhh-ahhh@users.noreply.github.com>
2022-06-19 15:42:17 +0100

    Update spice.lua

--- spice.lua
+++ spice.lua
@@ -1,8 +1,12 @@
 assert(not SWM, "Server already listening!");
 local hookEnabled;
 local socket = WebSocket.connect("ws://localhost:8291")
+local id = nil;
 
 local function hookedPrint(...)
+    if(id == nil) then 
+        return; 
+    end
 
 	local printResult = " "..os.date("*t")["hour"]..":"..os.date("*t")["min"]..":"..os.date("*t")["sec"].." -- ";
     for i,v in ipairs({...}) do
@@ -15,7 +19,9 @@ local function hookedPrint(...)
 end
 
 local function hookedWarn(...)
-
+    if(id == nil) then 
+        return; 
+    end
 	local printResult = "@@yellow@@ ";
     printResult ..= os.date("*t")["hour"]..":"..os.date("*t")["min"]..":"..os.date("*t")["sec"].." -- ";
     for i,v in ipairs({...}) do
@@ -27,18 +33,19 @@ local function hookedWarn(...)
     socket:Send("c_out "..printResult);
 end
 
-getgenv().SWM = {};
+local SWM = {};
+_G.SWM = SWM;
 
 SWM.Backup = {};
 
 SWM.Clear = function()
-	if(hookEnabled) then
+	if(hookEnabled and id != nil) then
 		socket:Send("c_cls()");
 	end
 end
 
-getgenv().cls = SWM.Clear();
-getgenv().clear = SWM.Clear();
+_G.cls = SWM.Clear();
+_G.clear = SWM.Clear();
 	
 SWM.print = hookedPrint;
 SWM.warn = hookedWarn;
@@ -47,7 +54,12 @@ SWM.Backup.print = hookfunction(print,SWM.print);
 SWM.Backup.warn = hookfunction(warn,SWM.warn);
 
 socket.OnMessage:Connect(function(msg)
-  loadstring(msg)();
+  if(string.split(msg, ' ')[1] == "uuid") then
+    id = string.split(msg, ' ')[2];
+    socket:Send("got "..id);
+  else
+    loadstring(msg)();
+  end;
 end);
 
 SWM.HookOutput = function()
@@ -62,8 +74,11 @@ SWM.DisableHook = function()
 	hookfunction(warn,SWM.Backup.warn);
 end
 
-SWM.Backup.print("Script-Ware M has loaded! Ported by AHHH.");
-SWM.Backup.warn("Currently, every call to print and warn will redirect to the Built-In Console.");
-SWM.Backup.print("To disable this, run SWM.DisableHook(). If you're missing your logs being baked into Script-Ware, run SWM.HookOutput().");
-SWM.Backup.print("\n\n\n");
-SWM.Backup.print("This does not redirect the rconsole library, feel free to add it yourself.");
+SWM.Backup.print([[
+    Script-Ware M has loaded! Ported by AHHH.
+    Currently, every call to print and warn will redirect to the Build-In Console.
+    To disable this, use SWM.DisableHook(). If you're missing your logs being baked into Script-Ware, run SWM.HookOutput().
+
+
+    This does not redirect the rconsole library, feel free to add it yourself.
+]]);
