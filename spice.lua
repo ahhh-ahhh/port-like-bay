d369ebd0a25b4e70c181933d30b043a2339a8181
AHHH <64360573+ahhh-ahhh@users.noreply.github.com>
2022-07-08 00:38:35 +0100

    Update spice.lua

--- spice.lua
+++ spice.lua
@@ -3,6 +3,17 @@ local hookEnabled;
 local socket = WebSocket.connect("ws://localhost:8291")
 local id = nil;
 
+
+--prapin/LuaBrainFuck
+getgenv().SWM.bfuck = function(s)
+  local subst = {["+"]="v=v+1 ", ["-"]="v=v-1 ", [">"]="i=i+1 ", ["<"]="i=i-1 ",
+    ["."] = "w(v)", [","]="v=r()", ["["]="while v~=0 do ", ["]"]="end "}
+  local env = setmetatable({ i=0, t=setmetatable({},{__index=function() return 0 end}),
+    r=function() return io.read(1):byte() end, w=function(c) io.write(string.char(c)) end }, 
+    {__index=function(t,k) return t.t[t.i] end, __newindex=function(t,k,v) t.t[t.i]=v end })
+  load(s:gsub("[^%+%-<>%.,%[%]]+",""):gsub(".", subst), "brainfuck", "t", env)()
+end;
+
 local function hookedPrint(...)
     
     if(id == nil) then 
